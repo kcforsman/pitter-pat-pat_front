281927a48722318ab077bc090b14c79ac9edfa5c
Kaitlin Forsman <kcforsman@outlook.com>
2018-07-25 13:06:50 -0700

    added alerts based on current score is the score components is pressed

--- components/Score.js
+++ components/Score.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import {TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {TouchableOpacity, Text, StyleSheet, Alert} from 'react-native';
 import PropTypes from 'prop-types';
 
 export default class Score extends React.Component {
   static propTypes = {
     score: PropTypes.number.isRequired,
   }
+
+  alertScore = () => {
+    if ( this.props.score <= 6 ) {
+      Alert.alert('Ah....No', 'Keep Trying');
+    } else if ( this.props.score >= 18 ) {
+      Alert.alert('Amazing', `You're ready for something new!`);
+    } else {
+      Alert.alert('Decent', `Looks like you're getting it`);
+    }
+  }
+
   render() {
     return (
-      <TouchableOpacity onPress={() => {}} style={{flexDirection: 'row', alignItems: 'center',}}>
+      <TouchableOpacity onPress={this.alertScore} style={{flexDirection: 'row', alignItems: 'center',}}>
         <Text style={styles.text}>SCORE: { this.props.score }</Text>
       </TouchableOpacity>
     )
