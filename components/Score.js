413785b38f493047b95f075753f28cff125a47fd
Kaitlin Forsman <kcforsman@outlook.com>
2018-07-26 02:08:51 -0700

    added different alerts that depending on the existing highScores and currentScore for a phase

--- components/Score.js
+++ components/Score.js
@@ -8,24 +8,63 @@ export default class Score extends React.Component {
     highScore: PropTypes.number.isRequired,
   }
 
-  alertScore = () => {
-    if ( this.props.score <= 6 ) {
-      Alert.alert('Ah....No', 'Keep Trying');
-    } else if ( this.props.score >= 18 ) {
-      Alert.alert('Amazing', `You're ready for something new!`);
+  alertScorePress = () => {
+    if (this.props.highScore == 0) {
+      if (this.props.score == 0) {
+        Alert.alert('Ah....No', `You haven't even started`);
+      } else if (this.props.score <= 6) {
+        Alert.alert('Ummm.....', `You're getting started. Keep going`);
+      } else if (this.props.score >= 18) {
+        Alert.alert('Amazing', `You got it on your first try`);
+      } else {
+        Alert.alert('Decent', `Looks like you're getting it on the first go`);
+      }
+    } else if (this.props.highScore <= 6) {
+      if (this.props.score <= 6) {
+        Alert.alert('Ah....No', `You're still learning`);
+      } else if (this.props.score >= 18) {
+        Alert.alert('Amazing', `You're really starting to get it!`);
+      } else {
+        Alert.alert('Decent', `That's definitely a good start!`);
+      }
+    } else if (this.props.highScore >= 18) {
+      if (this.props.score <= 6) {
+        Alert.alert('Ummm.....', `Well, your high score says you can do better`);
+      } else if (this.props.score >= 18) {
+        Alert.alert('Amazing', `You're ready for something new!`);
+      } else {
+        Alert.alert('Decent', `Looks like you're getting it. Maybe one more time.`);
+      }
     } else {
-      Alert.alert('Decent', `Looks like you're getting it`);
+      if (this.props.score <= 6) {
+        Alert.alert('Ummm.....', `Well, maybe you want a little more practice`);
+      } else if (this.props.score >= 18) {
+        Alert.alert('Amazing', `You're almost ready for the next level`);
+      } else {
+        Alert.alert('Decent', `It's definitely progress!`);
+      }
+    }
+
+  }
+
+  alertHighScorePress = () => {
+    if ( this.props.highScore <= 6 ) {
+      Alert.alert('Ah....No', 'Keep Trying');
+    } else if ( this.props.highScore >= 18 ) {
+      Alert.alert('Amazing', `That's an awesome High Score`);
+    } else {
+      Alert.alert('Decent', `Looks like you're starting to get it`);
     }
   }
 
   render() {
     return (
       <View style={{flexDirection: 'row',}}>
-        <TouchableOpacity onPress={this.alertScore} style={{flexDirection: 'row', alignItems: 'center',}}>
+        <TouchableOpacity onPress={this.alertScorePress} style={{flexDirection: 'row', alignItems: 'center',}}>
           <Text style={styles.text}>SCORE: { this.props.score }</Text>
         </TouchableOpacity>
         <View style={{width: 90}}></View>
-        <TouchableOpacity onPress={this.alertScore} style={{flexDirection: 'row', alignItems: 'center',}}>
+        <TouchableOpacity onPress={this.alertHighScorePress} style={{flexDirection: 'row', alignItems: 'center',}}>
           <Text style={styles.text}>HIGH SCORE: { this.props.highScore }</Text>
         </TouchableOpacity>
       </View>
